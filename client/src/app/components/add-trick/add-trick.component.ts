1aa412d30085e142dd90a80652460b7ea88c2e32
Vigonny Eugene <ugurm98@gmail.com>
2019-08-14 18:12:29 +0600

    Add&edit components

--- client/src/app/components/add-trick/add-trick.component.ts
+++ client/src/app/components/add-trick/add-trick.component.ts
@@ -1,24 +1,24 @@
-import { Trick } from "src/app/models/trick";
-import { TrickService } from "../../services/trick.service";
-import { Component } from "@angular/core";
-import { FormBuilder, Validators } from "@angular/forms";
-import { Location } from "@angular/common";
+import { Trick } from 'src/app/models/trick';
+import { TrickService } from '../../services/trick.service';
+import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
 
 @Component({
-  selector: "app-add-trick",
-  templateUrl: "./add-trick.component.html",
-  styleUrls: ["./add-trick.component.scss"],
+  selector: 'app-add-trick',
+  templateUrl: './add-trick.component.html',
+  styleUrls: ['./add-trick.component.scss'],
 })
 export class AddTrickComponent {
-  duplicateName: boolean = false;
+  duplicateName = false;
 
   trickForm = this.fb.group({
-    name: ["", [Validators.required, Validators.minLength(5)]],
+    name: ['', [Validators.required, Validators.minLength(5)]],
     complexity: [
-      "",
+      '',
       [Validators.required, Validators.min(1), Validators.max(1000)],
     ],
-    description: [""],
+    description: [''],
   });
 
   constructor(
@@ -34,7 +34,7 @@ export class AddTrickComponent {
         this.location.back();
       },
       err => {
-        if (err.status == 409) {
+        if (err.status === 409) {
           this.duplicateName = true;
         }
       },
