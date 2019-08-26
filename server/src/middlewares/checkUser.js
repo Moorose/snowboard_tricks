d7264bcfa0eebfebea99985bee6a4db63fd5202e
Vigonny Eugene <ugrum98@gmail.com>
2019-08-26 16:42:55 +0600

    rebase to mark-system-front

--- server/src/middlewares/checkUser.js
+++ server/src/middlewares/checkUser.js
@@ -1,11 +1,10 @@
-'use strict';
-const userService = require("../service/userService");
+
+const userService = require('../service/userService');
 
 exports.isUser = async (session) => {
   if (session) {
     const users = await userService.getUserList();
     if (users.length !== 0) {
-      console.log('Current user:', users[0].dataValues);
       return users[0];
     }
   }
